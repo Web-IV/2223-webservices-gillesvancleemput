d483b887e0dbb4add64d3bf29e7fcbac56558b74
Gilles Van Cleemput <gilles.vancleemput@student.hogent.com>
2022-11-17 12:29:41 +0100

    seeda

--- src/data/seeds/202211032203_menuItems.js
+++ src/data/seeds/202211032203_menuItems.js
@@ -7,7 +7,7 @@ module.exports = {
 		// then add the fresh places
 		await knex('menu').insert([
             {itemId: '00d1535a-b152-4d6c-bd50-b5d408f98948', naam: 'Wacko Burger', prijs: 14, type: 'Burgers & Wraps', beschrijving: 'vegan chicken, avocado, tomato, red onion, corn'},
-            {itemId: '097c33bf-01dd-4ae6-ad5c-73ef1630f0de', naam: 'Shoarma Mama Burger', prijs: 14, type: 'Burgers & Wraps', beschrijving: 'vegan shoarma, mixed salad, pomegranate, red onion, beetroot'},
+            {itemId: '097c33bf-01dd-4ae6-ad5c-73ef1630f0de', naam: 'ShoarMama Burger', prijs: 14, type: 'Burgers & Wraps', beschrijving: 'vegan shoarma, mixed salad, pomegranate, red onion, beetroot'},
             {itemId: '0ea6daae-45f0-4586-97c7-537aeab87c07', naam: 'Wacko Wrap', prijs: 13, type: 'Burgers & Wraps', beschrijving: 'vegan chicken, avocado, tomato, red onion, corn'},
             {itemId: '0f8c92a2-2290-459c-b094-12a48a27f925', naam: 'Shoarma Mama Wrap', prijs: 13, type: 'Burgers & Wraps', beschrijving: 'vegan shoarma, mixed salad, pomegranate, red onion, beetroot'},
             {itemId: '25c6b2d0-7b9a-400a-b17f-709b38ad2e80', naam: 'Tuna Bowl', prijs: 12, type: 'Sushibowls', beschrijving: 'vegan tuna, avocado, red onion, edame,lettuce, nori, crispy onion'},
@@ -22,10 +22,10 @@ module.exports = {
             {itemId: '9f3f438a-c11a-49b7-bcc5-a7f71f64b070', naam: 'Beer', prijs: 3.5, type: 'Drinks', beschrijving: ''}, 
             {itemId: 'b47f8d09-50fb-4d36-a007-b512dd6eb4f7', naam: 'Yugen ', prijs: 4, type: 'Drinks', beschrijving: ''}, 
             {itemId: 'bbcc801f-e388-416d-b0b6-0445374c83e8', naam: 'Lokales', prijs: 4, type: 'Drinks', beschrijving: ''},
-            {itemId: 'c40c2705-4547-4129-8e10-1d0493c557d4', naam: 'Sweet Chilli Mayo', prijs: 0, type: 'Sauces', beschrijving: ''},
-            {itemId: 'd45979ca-fd2b-428f-b5c7-56ed3cd41fb7', naam: 'Cocktail', prijs: 0, type: 'Sauces', beschrijving: ''},
-            {itemId: 'e2b6d0c8-6df2-4678-9beb-3e26b21adf70', naam: 'Wasabi Mayo', prijs: 0, type: 'Sauces', beschrijving: ''},
-            {itemId: 'e8678d0e-b741-4300-a4e4-977b90d621ed', naam: 'Curry', prijs: 0, type: 'Sauces', beschrijving: ''},
+            {itemId: 'c40c2705-4547-4129-8e10-1d0493c557d4', naam: 'Sweet Chilli Mayo', prijs: 0.5, type: 'Sauces', beschrijving: ''},
+            {itemId: 'd45979ca-fd2b-428f-b5c7-56ed3cd41fb7', naam: 'Cocktail', prijs: 0.5, type: 'Sauces', beschrijving: ''},
+            {itemId: 'e2b6d0c8-6df2-4678-9beb-3e26b21adf70', naam: 'Wasabi Mayo', prijs: 0.5, type: 'Sauces', beschrijving: ''},
+            {itemId: 'e8678d0e-b741-4300-a4e4-977b90d621ed', naam: 'Curry', prijs: 0.5, type: 'Sauces', beschrijving: ''},
             {itemId: 'ec114c5d-cb6d-41e8-985c-fe543385566b', naam: 'Shrimps ', prijs: 6, type: 'Sides', beschrijving: ''},
             {itemId: 'f9734f64-3acf-46a2-8cf9-a05df27608ac', naam: 'Sweet Patato Fries', prijs: 6, type: 'Sides', beschrijving: ''},
             {itemId: 'ffa1c41b-7d32-4cc5-b0fc-01f51bc5bd53', naam: 'Calamaresrings', prijs: 6, type: 'Sides', beschrijving: ''},     
